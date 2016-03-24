57a247830c81c3217feb22c3825a011f59bb4daa
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-03-24 15:32:36 -0400

    Update fish_user_key_bindings.fish

--- fish_user_key_bindings.fish
+++ fish_user_key_bindings.fish
@@ -95,3 +95,6 @@ function handle_input_bash_conditional --description 'Function used for binding 
 end
 
 function fish_user_key_bindings
+    bind \& 'handle_input_bash_conditional \&'
+    bind \| 'handle_input_bash_conditional \|'
+end
