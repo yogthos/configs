ff2dc2334faf1089baa1b8e1b94a7f3d98605d1c
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2017-02-13 13:47:44 -0500

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 if status --is-login
         set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home
         set -gx PATH $PATH ~/bin $JAVA_HOME ~/bin /usr/local/git/bin
+        set -gx NODE_PATH "/usr/local/lib/node_modules"
 end
 
 function fish_greeting
