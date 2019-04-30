704e877d5f0a52a4f0cd477a9e9ba60e17c18ad4
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2019-04-29 21:29:21 -0400

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
 if status --is-login
-        set -gx JAVA_HOME (/usr/libexec/java_home -v 1.8)
+        set -gx JAVA_HOME (/usr/libexec/java_home -v 11)
         set -gx PATH $PATH $JAVA_HOME ~/bin /usr/local/git/bin
         set -gx NODE_PATH "/usr/local/lib/node_modules"
 end
